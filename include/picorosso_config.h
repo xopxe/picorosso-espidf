57a0a82308d07ef94fe3f043d40cb7893896fb0d
Jorge <xxopxe@gmail.com>
2025-09-19 17:08:32 -0300

    remove Remove unused Defines (moved to esp_log)

--- include/picorosso_config.h
+++ include/picorosso_config.h
@@ -2,31 +2,18 @@
 #define __PICOROSSO_CONFIG_H
 
 /*
- Serial console used for debug output. If commented, no output is prdouced.
- To send to USB serial, set DEBUG_CONSOLE to Serial and PINs to -1.
-*/
-
-#define DEBUG_CONSOLE Serial
-#define DEBUG_CONSOLE_PIN_RX -1
-#define DEBUG_CONSOLE_PIN_TX -1
-#define DEBUG_CONSOLE_BAUD 115200
-
-/*
-#define DEBUG_CONSOLE Serial1
-#define DEBUG_CONSOLE_PIN_RX 9
-#define DEBUG_CONSOLE_PIN_TX 10
-#define DEBUG_CONSOLE_BAUD 115200
-*/
-
-/*
  Size of the pre-allocated buffer for serialization
 */
+#if !defined(PUBLISHER_BUF_SIZE)
 #define PUBLISHER_BUF_SIZE 1024
+#endif
 
 /*
 Use sync_time node to get synchronize
 */
+#if !defined(USE_SYNC_TIME)
 #define USE_SYNC_TIME true
+#endif
 
 
 #endif // __PICOROSSO_CONFIG_H
